a59fb0aaaa1b4cce60c3637fb95b7a2282110180
William Roy <wroy@proton.me>
2023-12-12 10:40:43 -0500

    Load targets as plugins for gobject introspection

--- echo/echo.cxx
+++ echo/echo.cxx
@@ -1,16 +1,25 @@
-import std;
-import echo;
+#include <libpeas.h>
 
 int
 main (int argc, char *argv[])
 {
-  using namespace std;
+  const auto e (peas_engine_get_default ());
+  const auto d ("/home/wroy/Projects/libecho");
 
-  if (argc < 2)
+  // TODO: Relocatable installation.
+  //
+  peas_engine_add_search_path (e, d, d);
+
+  // Load targets through libpeas for GObject introspection.
+  //
+  for (guint i (0); i < g_list_model_get_n_items (G_LIST_MODEL (e)); i++)
     {
-      cerr << "error: missing name" << endl;
-      return 1;
-    }
+      g_autoptr (PeasPluginInfo) p
+          = PEAS_PLUGIN_INFO (g_list_model_get_item (G_LIST_MODEL (e), i));
 
-  echo::say_hello (argv[1]);
+      g_message ("Loading plugin: %s",
+                 peas_plugin_info_get_name (p));
+
+      peas_engine_load_plugin (e, p);
+    }
 }
