5fe73a157f68acbb1f520e004b25e8a9c6d5c66d
William Roy <wroy@proton.me>
2024-01-06 20:05:26 -0500

    Update copyright year (2024)

--- libecho/export.h
+++ libecho/export.h
@@ -1,31 +1,21 @@
 #pragma once
 
-#if defined(LIBECHO_STATIC)         /* Using static. */
+#if defined(LIBECHO_STATIC)
 #  define LIBECHO_SYMEXPORT
-#elif defined(LIBECHO_STATIC_BUILD) /* Building static. */
+#elif defined(LIBECHO_STATIC_BUILD)
 #  define LIBECHO_SYMEXPORT
-#elif defined(LIBECHO_SHARED)       /* Using shared. */
+#elif defined(LIBECHO_SHARED)
 #  ifdef _WIN32
 #    define LIBECHO_SYMEXPORT __declspec(dllimport)
 #  else
 #    define LIBECHO_SYMEXPORT
 #  endif
-#elif defined(LIBECHO_SHARED_BUILD) /* Building shared. */
+#elif defined(LIBECHO_SHARED_BUILD)
 #  ifdef _WIN32
 #    define LIBECHO_SYMEXPORT __declspec(dllexport)
 #  else
 #    define LIBECHO_SYMEXPORT
 #  endif
 #else
-/* If none of the above macros are defined, then we assume we are being used
- * by some third-party build system that cannot/doesn't signal the library
- * type. Note that this fallback works for both static and shared libraries
- * provided the library only exports functions (in other words, no global
- * exported data) and for the shared case the result will be sub-optimal
- * compared to having dllimport. If, however, your library does export data,
- * then you will probably want to replace the fallback with the (commented
- * out) error since it won't work for the shared case.
- */
-#  define LIBECHO_SYMEXPORT         /* Using static or shared. */
-/*#  error define LIBECHO_STATIC or LIBECHO_SHARED preprocessor macro to signal libecho library type being linked */
+#  define LIBECHO_SYMEXPORT
 #endif
