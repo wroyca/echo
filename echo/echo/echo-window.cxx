4a9460ef716631ba08a7338b34edceedbffb1e20
William Roy <wroy@proton.me>
2024-03-05 17:26:23 -0500

    feat: Zydis disassembly sample

--- echo/echo/echo-window.cxx
+++ echo/echo/echo-window.cxx
@@ -27,8 +27,9 @@ struct _EchoWindow
 	AdwApplicationWindow  parent_instance;
 
 	/* Template widgets */
-	AdwHeaderBar        *header_bar;
-	GtkLabel            *label;
+	AdwHeaderBar         *header_bar;
+	GtkTextView          *text_view;
+	GtkTextBuffer        *text_buffer;
 };
 
 G_DEFINE_FINAL_TYPE (EchoWindow, echo_window, ADW_TYPE_APPLICATION_WINDOW)
@@ -40,11 +41,79 @@ echo_window_class_init (EchoWindowClass *klass)
 
 	gtk_widget_class_set_template_from_resource (widget_class, "/app/drey/Echo/echo-window.ui");
 	gtk_widget_class_bind_template_child (widget_class, EchoWindow, header_bar);
-	gtk_widget_class_bind_template_child (widget_class, EchoWindow, label);
+	gtk_widget_class_bind_template_child (widget_class, EchoWindow, text_view);
+	gtk_widget_class_bind_template_child (widget_class, EchoWindow, text_buffer);
 }
 
 static void
 echo_window_init (EchoWindow *self)
 {
 	gtk_widget_init_template (GTK_WIDGET (self));
+
+#if ECHO_DEVELOP
+  gtk_widget_add_css_class (GTK_WIDGET (self), "devel");
+#endif
+
+  // Sample ELF disassembly to get a feel for where we're headed.
+  //
+  void
+  sample (EchoWindow *self);
+  sample (self);
+}
+
+#include <LIEF/LIEF.hpp>
+#include <Zydis/Zydis.h>
+#include <Zycore/Format.h>
+
+void
+sample (EchoWindow *self)
+{
+  using namespace std;
+  using namespace LIEF::ELF;
+
+  ZydisDecoder decoder;
+  ZydisDecoderInit(&decoder, ZYDIS_MACHINE_MODE_LONG_64, ZYDIS_STACK_WIDTH_64);
+  ZydisFormatter fmt;
+  ZydisFormatterInit(&fmt, ZYDIS_FORMATTER_STYLE_INTEL);
+  ZydisDecodedInstruction instr;
+  ZydisDecodedOperand operands[ZYDIS_MAX_OPERAND_COUNT];
+
+  auto elf = Parser::parse ("/home/wroy/Documents/sample");
+  auto elf_text_section (elf->get_section (".text"));
+  auto elf_text_section_content (elf_text_section->content());
+  auto elf_text_section_content_data (elf_text_section_content.data());
+  auto elf_text_section_content_size (elf_text_section_content.size());
+  auto elf_text_section_content_instruction (vector <string>{});
+
+  ZyanU64 addr (0x007FFFFFFF400000);
+  ZyanUSize offset (elf_text_section->virtual_address());
+
+  while (ZYAN_SUCCESS(ZydisDecoderDecodeFull(&decoder,
+          elf_text_section_content_data,
+          elf_text_section_content_size,
+          &instr,
+          operands)))
+  {
+    char buffer[256];
+    ZydisFormatterFormatInstruction (&fmt,
+        &instr,
+        operands,
+        instr.operand_count_visible,
+        buffer,
+        sizeof (buffer),
+        addr,
+        nullptr);
+
+    elf_text_section_content_instruction.push_back(buffer);
+    elf_text_section_content_data += instr.length;
+    elf_text_section_content_size -= instr.length;
+  }
+
+  GtkTextIter iter;
+  for (auto &instruction : elf_text_section_content_instruction)
+  {
+    gtk_text_buffer_get_end_iter(self->text_buffer, &iter);
+    gtk_text_buffer_insert(self->text_buffer, &iter, instruction.c_str(), -1);
+    gtk_text_buffer_insert(self->text_buffer, &iter, "\n", -1);
+  }
 }
